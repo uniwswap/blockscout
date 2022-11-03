5425a5c87f2e8cebd89477ab04171de6da0e3a67
ravirasadiya <ravirasadiya1994@gmail.com>
2022-11-03 16:12:59 +0530

    update

--- apps/explorer/lib/explorer/chain/internal_transaction.ex
+++ apps/explorer/lib/explorer/chain/internal_transaction.ex
@@ -443,7 +443,6 @@ defmodule Explorer.Chain.InternalTransaction do
     changeset
     |> cast(attrs, @call_allowed_fields)
     |> validate_required(@call_required_fields)
-    |> validate_call_error_or_result()
     |> check_constraint(:call_type, message: ~S|can't be blank when type is 'call'|, name: :call_has_call_type)
     |> check_constraint(:input, message: ~S|can't be blank when type is 'call'|, name: :call_has_call_type)
     |> foreign_key_constraint(:from_address_hash)
@@ -460,6 +459,7 @@ defmodule Explorer.Chain.InternalTransaction do
     changeset
     |> cast(attrs, @create_allowed_fields)
     |> validate_required(@create_required_fields)
+    |> validate_create_error_or_result()
     |> check_constraint(:init, message: ~S|can't be blank when type is 'create'|, name: :create_has_init)
     |> foreign_key_constraint(:created_contract_address_hash)
     |> foreign_key_constraint(:from_address_hash)
